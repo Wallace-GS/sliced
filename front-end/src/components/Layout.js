374dee93aad20684a16b02e99eb94238a7b56c9e
Wallace-GS <wgsantos29@gmail.com>
2020-10-11 10:58:55 -0700

    adds global styles and typography

--- front-end/src/components/Layout.js
+++ front-end/src/components/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Footer } from './Footer';
 import { Nav } from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export const Layout = ({ children }) => {
   let temp;
 
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
+
       <Nav />
       {children}
       <Footer />
